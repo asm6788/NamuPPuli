c1d4ffd6d6566c4ef0b3a1386977383423bbb15b
asm6788 <asm6788@naver.com>
2022-09-20 01:57:52 +0900

    | 처리

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use serde::Deserialize;
 use std::fs;
 
@@ -6,12 +7,32 @@ struct NamuWiki {
     title: String,
     text: String,
 }
-fn main() {
-    let data = fs::read_to_string(r"F:\namuwiki210301\namuwiki_20210301.json")
-        .expect("Unable to read file");
-    let out: Vec<NamuWiki> = serde_json::from_str(&data).unwrap();
 
+fn remove_suffix<'a>(s: &'a str, p: &str) -> &'a str {
+    match s.find(p) {
+        Some(index) => &s[..index],
+        None => s,
+    }
+}
+
+fn main() {
+    let out: Vec<NamuWiki>;
+    {
+        let data = fs::read_to_string(r"F:\namuwiki210301\namuwiki_20210301.json")
+            .expect("Unable to read file");
+        out = serde_json::from_str(&data).unwrap();
+    }
+    let href = Regex::new(r"\[\[(.*?)\]\]").unwrap();
     for data in out.iter() {
-        println!("{:#?} 의 내용\n{:#?}", data.title, data.text);
+        for caps in href.captures_iter(&data.text) {
+            if caps[1].contains("파일:")
+                || caps[1].contains("분류:")
+                || caps[1].contains("틀:")
+                || caps[1].starts_with("http")
+            {
+                continue;
+            }
+            println!("{}", remove_suffix(&caps[1], "|"));
+        }
     }
 }
