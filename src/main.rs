33f7f8e6a711354de8b274f99ae649be7cd675f7
asm6788 <asm6788@naver.com>
2022-09-21 01:55:32 +0900

    파서 직접구현/메모리 대폭절약

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,65 @@
-use regex::Regex;
-use serde::Deserialize;
-use std::collections::HashMap;
-use std::fs;
+use rustc_hash::FxHashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::{str, thread, time};
 
-#[derive(Deserialize, Debug)]
-struct NamuWiki {
-    title: String,
-    text: String,
+fn read_n<R>(reader: R, bytes_to_read: u64) -> Vec<u8>
+where
+    R: Read,
+{
+    let mut buf = vec![];
+    let mut chunk = reader.take(bytes_to_read);
+    // Do appropriate error handling for your situation
+    // Maybe it's OK if you didn't read enough bytes?
+    let n = chunk.read_to_end(&mut buf).expect("Didn't read enough");
+    assert_eq!(bytes_to_read as usize, n);
+    buf
 }
 
 fn remove_suffix<'a>(s: &'a str, p: &str) -> &'a str {
     match s.find(p) {
         Some(index) => &s[..index],
-        None => s,
+        None => &s,
     }
 }
 
 fn main() {
-    let out: Vec<NamuWiki>;
-    {
-        let data =
-            fs::read_to_string(r"/home/asm6788/NamuPPuli/test.json").expect("Unable to read file");
-        out = serde_json::from_str(&data).unwrap();
-    }
-    let href = Regex::new(r"\[\[(.*?)\]\]").unwrap();
+    let mut reader = BufReader::new(File::open(r"test.json").unwrap());
+    let mut counter: FxHashMap<String, u16> = FxHashMap::default();
+    let mut record = vec![];
+    let mut do_record = false;
+    let mut buf = vec![0, 0];
+    loop {
+        buf.rotate_left(1);
+        buf[1] = read_n(&mut reader, 1)[0];
 
-    let mut counter: HashMap<String, u16> = HashMap::new();
-    for data in out.iter() {
-        for caps in href.captures_iter(&data.text) {
-            if caps[1].starts_with("파일:")
-                || caps[1].starts_with("분류:")
-                || caps[1].starts_with("틀:")
-                || caps[1].starts_with("http")
-            {
-                continue;
+        if buf[0] == b'[' && buf[1] == b'[' {
+            do_record = true;
+        } else if buf[0] == b']' && buf[1] == b']' {
+            do_record = false;
+        }
+
+        if do_record {
+            record.push(buf[1]);
+        } else {
+            if record.len() > 0 {
+                let mut href = str::from_utf8(&record).unwrap();
+                href = &href[1..href.len() - 1];
+                if href.starts_with("파일:")
+                    || href.starts_with("분류:")
+                    || href.starts_with("틀:")
+                    || href.starts_with("http")
+                {
+                    record.clear();
+                    continue;
+                }
+
+                href = remove_suffix(remove_suffix(&href, "|"), "#");
+                *counter.entry(String::from(href)).or_insert(0) += 1;
+                println!("{}가 {}번 참고됨", href, counter[&String::from(href)]);
+                record.clear();
             }
-            let href = remove_suffix(remove_suffix(&caps[1], "|"), "#");
-            *counter.entry(String::from(href)).or_insert(0) += 1;
         }
     }
-    let mut hash_vec: Vec<(&String, &u16)> = counter.iter().collect();
-    hash_vec.sort_by(|a, b| b.1.cmp(a.1));
-
-    println!("{:?}", hash_vec);
 }
