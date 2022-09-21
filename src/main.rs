c91499170be6df014b677766fc0a1fed1d0d557e
asm6788 <asm6788@naver.com>
2022-09-21 22:14:00 +0900

    램에 적재후 처리하도록 수정 / csv 형식으로 출력

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::SeekFrom;
 use std::str;
 use std::sync::Arc;
 use std::thread;
@@ -33,7 +31,10 @@ fn remove_suffix<'a>(s: &'a str, p: &str) -> &'a str {
 }
 
 fn main() {
-    let file = File::open(r"test.json").unwrap();
+    let mut file = File::open(r"test.json").unwrap();
+    let mut buf = Arc::new(Vec::new());
+    file.read_to_end(Arc::get_mut(&mut buf).unwrap()).unwrap();
+
     let pb = Arc::new(ProgressBar::new(file.metadata().unwrap().len()));
 
     pb.set_style(
@@ -45,7 +46,7 @@ fn main() {
     );
 
     let counter_d = DashMap::new();
-    let counter: Arc<DashMap<String, u16>> = Arc::new(counter_d);
+    let counter: Arc<DashMap<String, u32>> = Arc::new(counter_d);
 
     let thread_count = num_cpus::get() as u64;
     let each_size = file.metadata().unwrap().len() / thread_count;
@@ -53,23 +54,23 @@ fn main() {
     for i in 0..thread_count {
         let counter = counter.clone();
         let pb = pb.clone();
+        let mut index = 0;
+        let buf = buf.clone();
         let current_job = thread::spawn(move || {
-            let mut reader = BufReader::new(File::open(r"test.json").unwrap());
-            reader.seek(SeekFrom::Start(each_size * i)).unwrap();
-            let mut buf = vec![0, 0];
             let mut record = vec![];
             let mut do_record = false;
             loop {
                 pb.inc(1);
-                if reader.seek(SeekFrom::Current(0)).unwrap() == each_size * (i + 1) {
+
+                if (each_size * i + index + 1) as usize >= buf.len()
+                    || each_size * i + index + 1 >= each_size * (i + 1)
+                {
                     break;
                 }
 
-                buf.rotate_left(1);
-                buf[1] = match read_n(&mut reader, 1) {
-                    Ok(v) => v[0],
-                    Err(_) => break,
-                };
+                let buf =
+                    &buf[(each_size * i + index) as usize..=(each_size * i + index + 1) as usize];
+                index += 1;
 
                 if buf[0] == b'[' && buf[1] == b'[' {
                     do_record = true;
@@ -102,10 +103,10 @@ fn main() {
             current_job.join().unwrap();
         }
     }
-
+    pb.finish();
     let mut hash_vec: Vec<_> = counter.iter().collect();
     hash_vec.sort_by(|a, b| b.cmp(a));
     for line in hash_vec.into_iter() {
-        println!("{}: {}", line.key(), line.value());
+        println!("\"{}\",{}", line.key(), line.value());
     }
 }
